4d157d302361ffe1c4d9bb9bb3fa44e6b684c9b9
Bastien Van Houdt <bastienvanhoudt@gmail.com>
2022-09-19 09:07:57 +0200

    Add class for students with defaul tasks values

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+import { student } from './models.js';
+
 console.log('Welcome to the tracker');
+
+let student1 = new student('Franske');
+console.log(student1.name);
+console.log(student1.getTasks);
---
+++ src/models.js
@@ -0,0 +1,24 @@
+class student {
+  constructor(name) {
+    this.name = name;
+  }
+  
+  #tasks = {
+    task1: 'waiting',
+    task2: 'waiting',
+    task3: 'waiting',
+    task4: 'waiting',
+    task5: 'waiting',
+    task6: 'waiting',
+    task7: 'waiting',
+    task8: 'waiting',
+    task9: 'waiting',
+    task10: 'waiting',
+  }
+
+  get getTasks() {
+    return this.#tasks;
+  }
+}
+
+export { student };
